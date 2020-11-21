ea52fa0ed64b3338083102e7832e0ab56900cfd6
calvin <puram.calvin@gmail.com>
2020-11-21 13:50:04 +0100

    setup auth functionality

---
+++ src/tests/users.test.ts
@@ -0,0 +1,63 @@
+import * as request from "supertest";
+import * as mongoose from "mongoose";
+import Users from "../models/user.model";
+import User from "../interface/user.interface";
+import App from "../app";
+import UserRoute from "../routes/user.routes";
+import HttpException from "../exceptions/HttpException";
+
+const userRoute = new UserRoute();
+const app = new App([userRoute]);
+describe("Testing Users", () => {
+  let userData: User = {
+    _id: mongoose.Types.ObjectId(),
+    name: "calvin",
+    email: "xyz@gmail.com",
+    password: "2begood4",
+    confirmPassword: "2begood4",
+  };
+  let user: User;
+  let token: string;
+  beforeAll(async () => {
+    user = await Users.create(userData);
+    token = await user.sendToken();
+  });
+
+  afterAll(async () => {
+    await Users.deleteOne({ name: "calvin" });
+    await mongoose.connection.close();
+  });
+
+  describe("[GET] /users", () => {
+    it("should get all the users", async () => {
+      const res = await request(app.getServer())
+        .get(`${userRoute.path}s`)
+        .set("authorization", `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBeTruthy();
+    });
+  });
+
+  describe("[GET] /user", () => {
+    it("should get a single user", async () => {
+      const res = await request(app.getServer())
+        .get(`${userRoute.path}/${user._id}`)
+        .set("authorization", `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBeTruthy();
+    });
+  });
+
+  describe("[DELETE] /user/:id", () => {
+    it("should delete a user", async () => {
+      const res = await request(app.getServer())
+        .get(`${userRoute.path}/${user._id}`)
+        .set("authorization", `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBeTruthy();
+    });
+  });
+});
